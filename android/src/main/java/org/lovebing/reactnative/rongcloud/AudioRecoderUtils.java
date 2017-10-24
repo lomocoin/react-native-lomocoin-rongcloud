c1e41e33eb054fcedb9856d4ca1cd5bab0cd6252
qiaojiayan <26757270@qq.com>
2017-10-24 16:01:44 +0800

    录音修改

--- android/src/main/java/org/lovebing/reactnative/rongcloud/AudioRecoderUtils.java
+++ android/src/main/java/org/lovebing/reactnative/rongcloud/AudioRecoderUtils.java
@@ -64,7 +64,7 @@ public class AudioRecoderUtils {
             /* ②setAudioSource/setVedioSource */
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);// 设置麦克风
             /* ②设置音频文件的编码：AAC/AMR_NB/AMR_MB/Default 声音的（波形）的采样 */
-            mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+            mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
             /*
              * ②设置输出文件的格式：THREE_GPP/MPEG-4/RAW_AMR/Default THREE_GPP(3gp格式
              * ，H263视频/ARM音频编码)、MPEG-4、RAW_AMR(只支持音频且音频编码要求为AMR_NB)
