312bbe073852af8b41b72d86747c485304970e93
qiaojiayan <2628747@qq.com>
2018-05-31 08:56:43 +0800

    IMPROVE(RongNotificationReceiver.java) add otc push jump

--- android/src/main/java/com/lomocoin/imlib/RongNotificationReceiver.java
+++ android/src/main/java/com/lomocoin/imlib/RongNotificationReceiver.java
@@ -1,8 +1,12 @@
 package com.lomocoin.imlib;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
+import io.rong.imlib.ipc.RongService;
+import io.rong.push.RongPushClient;
 import io.rong.push.notification.PushMessageReceiver;
 import io.rong.push.notification.PushNotificationMessage;
 
@@ -13,12 +17,26 @@ import io.rong.push.notification.PushNotificationMessage;
 public class RongNotificationReceiver extends PushMessageReceiver {
     @Override
     public boolean onNotificationMessageArrived(Context context, PushNotificationMessage pushNotificationMessage) {
-        Log.e("isme","rong msg init");
+        Log.e("isme", "rong msg init");
         return false;
     }
 
     @Override
-    public boolean onNotificationMessageClicked(Context context, PushNotificationMessage pushNotificationMessage) {
+    public boolean onNotificationMessageClicked(Context context, PushNotificationMessage msg) {
+        Log.e("isme", "rong msg click");
+
+        // fait otc
+        if (msg.getPushData() != null && msg.getPushData().length() > 0) {
+            String pushData = msg.getPushData();
+            if (pushData.contains("FiatOrder") && pushData.contains("orderId")) {
+                Intent intent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+                intent.putExtra("action", "fiat_otc_push");
+                intent.putExtra("data", pushData);
+                context.startActivity(intent);
+                return true;
+            }
+        }
+
         return false;
     }
 }
